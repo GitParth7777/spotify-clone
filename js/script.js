b15a436561756445c50504831f2f4955568ce24b
Parth Babariya <parth.babariya8849@gmail.com>
2025-06-30 18:41:44 +0530

    Make songs and album data work on GitHub Pages

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ const playMusic = (track, pause = false) => {
 
 
 async function displayAlbums() {
-    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let a = await fetch("./songs/");
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -104,7 +104,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
             //get metadata
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+            let a = await fetch(`./${folder}/info.json`);
             let response = await a.json();
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
